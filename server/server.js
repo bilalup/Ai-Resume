c62e6c349c44c47ad9595949403af9030b32d035
Siraj Mohammed <bstech500@gmail.com>
2025-09-29 22:24:31 +0300

    initial commit

--- server/server.js
+++ server/server.js
@@ -33,5 +33,5 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.log(err));
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
